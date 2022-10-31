784853a3a169502981a1facb6d97dfe021ca9593
megalocoman <cfjofre@uc.cl>
2022-10-31 11:37:11 -0300

    update owners, visit and speciality

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jt on 7/13/18.
  */
 public class Vet extends Person {
+    private Set<Specialty> specialty;
+
+    public Set<Specialty> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Set<Specialty> specialty) {
+        this.specialty = specialty;
+    }
 }
