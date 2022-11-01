b4eb946be9b67e5f5f805741d957062638079802
megalocoman <cfjofre@uc.cl>
2022-11-01 09:00:58 -0300

    specialties aimpl

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,12 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jt on 7/13/18.
  */
 public class Vet extends Person {
-    private Set<Specialty> specialty;
+    private Set<Specialty> specialty = new HashSet<>();
 
     public Set<Specialty> getSpecialty() {
         return specialty;
