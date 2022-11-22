d74b72d75a82f5b147727929060e25628eaf753c
megalocoman <cfjofre@uc.cl>
2022-11-21 22:30:29 -0300

    owner entity with jap annotation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,15 +1,24 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by jt on 7/13/18.
  */
+@Entity
+@Table(name="pet")
 public class Pet extends BaseEntity{
 
+    @ManyToOne
+    @JoinColumn(name="type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name="owner_id")
     private Owner owner;
+    @Column(name =  "birth_date")
     private LocalDate birthDate;
+    @Column(name = "name" )
     private String name;
 
     public PetType getPetType() {
